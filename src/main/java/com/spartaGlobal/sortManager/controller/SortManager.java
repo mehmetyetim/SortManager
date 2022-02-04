4435b57d4d0632fdca49e9389cdd35dbd32e0fce
mehmetyetim <yetimme@yahoo.com>
2022-02-04 16:59:46 +0000

    SoftManager.java added

--- src/main/java/com/spartaGlobal/sortManager/controller/SortManager.java
+++ src/main/java/com/spartaGlobal/sortManager/controller/SortManager.java
@@ -1,4 +1,25 @@
 package com.spartaGlobal.sortManager.controller;
 
+import com.spartaGlobal.sortManager.model.BubbleSortFactory;
+import com.spartaGlobal.sortManager.model.MergeSortFactory;
+import com.spartaGlobal.sortManager.model.Sorter;
+import com.spartaGlobal.sortManager.model.SorterFactory;
+import static com.spartaGlobal.sortManager.controller.ArrayGenerateManager.*;
+
 public class SortManager {
+
+    public int[] initiateSorting(String desiredSorterType, int desiredArrayLength){
+        Sorter s = getSorter(desiredSorterType);
+        int[] sortedArray = s.sort(getInstance().getArray(desiredArrayLength));
+        return sortedArray;
+    }
+
+    public static Sorter getSorter(String sorterType){
+        SorterFactory sf = switch (sorterType.toLowerCase()){
+            case "bubble" -> new BubbleSortFactory();
+            case "merge" -> new MergeSortFactory();
+            default -> null;
+        };
+        return sf.getInstance();
+    }
 }
