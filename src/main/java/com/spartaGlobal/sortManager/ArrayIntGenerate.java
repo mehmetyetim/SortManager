347e2ebeb2f0f9d62dcbc42a3916a5ac57ea81d3
mehmetyetim <yetimme@yahoo.com>
2022-02-04 12:28:46 +0000

    ArrayIntGenerate class added

---
+++ src/main/java/com/spartaGlobal/sortManager/ArrayIntGenerate.java
@@ -0,0 +1,14 @@
+package com.spartaGlobal.sortManager;
+
+import java.util.Random;
+
+public class ArrayIntGenerate {
+    public int[] generate(int length){
+        int[] array = new int[length];
+        Random random = new Random();
+        for(int i = 0;i<array.length;i++){
+            array[i] = random.nextInt(100);
+        }
+        return array;
+    }
+}
--- src/main/java/com/spartaGlobal/sortManager/Main.java
+++ src/main/java/com/spartaGlobal/sortManager/Main.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = {1,7,10,19,2,4,7,34,20,3,89};
-        int[] array2 = array.clone();
+        int[] array = new ArrayIntGenerate().generate(12);
+        int[] array2 = new ArrayIntGenerate().generate(8);
         System.out.println(Arrays.toString(array));
         System.out.println(Arrays.toString(new BubbleSort().sort(array)));
         System.out.println(Arrays.toString(array2));
