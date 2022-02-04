e19f25635130650f64b9eb402552334ac1280a7f
mehmetyetim <yetimme@yahoo.com>
2022-02-04 12:19:12 +0000

    Merge sort added

--- src/main/java/com/spartaGlobal/sortManager/Main.java
+++ src/main/java/com/spartaGlobal/sortManager/Main.java
@@ -5,7 +5,10 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] array = {1,7,10,19,2,4,7,34,20,3,89};
+        int[] array2 = array.clone();
         System.out.println(Arrays.toString(array));
         System.out.println(Arrays.toString(new BubbleSort().sort(array)));
+        System.out.println(Arrays.toString(array2));
+        System.out.println(Arrays.toString(new MergeSort().sort(array2)));
     }
 }
