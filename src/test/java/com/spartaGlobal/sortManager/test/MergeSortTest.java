4d04346ebfdc672d1035025a604fbeee8400c663
mehmetyetim <yetimme@yahoo.com>
2022-02-06 02:55:03 +0000

    MergeSortTest.java added

---
+++ src/test/java/com/spartaGlobal/sortManager/test/MergeSortTest.java
@@ -0,0 +1,36 @@
+package com.spartaGlobal.sortManager.test;
+
+import com.spartaGlobal.sortManager.controller.ArrayGenerateManager;
+import com.spartaGlobal.sortManager.controller.SortManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class MergeSortTest {
+
+    SortManager sortManager = new SortManager();
+
+    @ParameterizedTest
+    @ValueSource(ints = {2,5,6,99,120,67,21,0,1,4})
+    @DisplayName("Given list of integer values to length of Array Then Sorting")
+    public void givenListOfIntegerToLengthOfArrayThenSorting(int length){
+        int[] actual = ArrayGenerateManager.getInstance().getArray(length);
+        sortManager.initiateSorting("merge",length);
+        for (int i = 1; i < actual.length; i++) {
+            if (actual[i-1] > actual[i]) {
+                Assertions.fail();
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("Given negative number to initiate array")
+    public void givenNegativeNumberToInitiateArray(){
+        NegativeArraySizeException thrown = Assertions.assertThrows(NegativeArraySizeException.class, () -> {
+            int[] actual = ArrayGenerateManager.getInstance().getArray(-1);
+            sortManager.initiateSorting("bubble", -1);
+        });
+    }
+}
