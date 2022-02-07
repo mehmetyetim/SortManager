02f90f31bc11db3bc78d5ad7d9f78ca9568e555c
mehmetyetim <yetimme@yahoo.com>
2022-02-07 08:45:14 +0000

    SortManager.java updated

--- src/main/java/com/spartaGlobal/sortManager/controller/SortManager.java
+++ src/main/java/com/spartaGlobal/sortManager/controller/SortManager.java
@@ -1,28 +1,46 @@
 package com.spartaGlobal.sortManager.controller;
 
 import com.spartaGlobal.sortManager.model.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import static com.spartaGlobal.sortManager.controller.ArrayGenerateManager.*;
 
 public class SortManager {
 
+    private static Logger logger = LogManager.getLogger(SortManager.class.getName());
+
     public int[] initiateSorting(String desiredSorterType, int desiredArrayLength){
         Sorter s = getSorter(desiredSorterType);
+        logger.info("User initiated sorting");
         return s.sort(getInstance().getArray(desiredArrayLength));
     }
 
-    public long initiateRunning(String desiredSorterType, int desiredArrayLength){
+    public String initiateRunning(String desiredSorterType, int desiredArrayLength){
         Sorter s = getSorter(desiredSorterType);
-        return s.runningTime(getInstance().getArray(desiredArrayLength));
+        logger.info("User initiates timing");
+        return "Running time for " + desiredSorterType + " is " + s.runningTime(getInstance().getArray(desiredArrayLength));
     }
 
     public static Sorter getSorter(String sorterType){
-        SorterFactory sf = switch (sorterType.toLowerCase()){
-            case "bubble" -> new BubbleSortFactory();
-            case "merge" -> new MergeSortFactory();
-            case "binary" -> new BinarySearchTreeFactory();
-            default -> null;
-        };
+        SorterFactory sf = null;
+        switch (sorterType.toLowerCase()){
+            case "bubble" -> {
+                sf = new BubbleSortFactory();
+                logger.info("User chose bubble sort");
+            }
+            case "merge" -> {
+                sf = new MergeSortFactory();
+                logger.info("User chose merge sort");
+            }
+            case "binary" -> {
+                sf = new BinarySearchTreeFactory();
+                logger.info("User chose binary sort");
+            }
+            default -> {
+                logger.warn("User entered invalid sort");
+            }
+        }
         return sf.getInstance();
     }
 }
