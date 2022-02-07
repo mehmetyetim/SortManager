8c1e748d0d1e40211fba65aa96d65e4160b3c3bb
mehmetyetim <yetimme@yahoo.com>
2022-02-07 08:44:08 +0000

    ArrayGenerateManager.java fixed

--- src/main/java/com/spartaGlobal/sortManager/controller/ArrayGenerateManager.java
+++ src/main/java/com/spartaGlobal/sortManager/controller/ArrayGenerateManager.java
@@ -3,9 +3,11 @@ package com.spartaGlobal.sortManager.controller;
 import com.spartaGlobal.sortManager.model.ArrayGenerator;
 import com.spartaGlobal.sortManager.model.ArrayGeneratorFactory;
 import com.spartaGlobal.sortManager.model.ArrayIntGenerateFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ArrayGenerateManager {
-
+    private static Logger logger = LogManager.getLogger(ArrayGenerateManager.class.getName());
     int[] array;
     int[] copyOfArray;
 
@@ -23,6 +25,7 @@ public class ArrayGenerateManager {
     public int[] getUnsortedArray(){return copyOfArray;}
 
     public int[] getArray(int desiredArrayLength){
+        logger.info("User created array with desired array length");
         if(array == null) {
             this.array = getArrayGenerator().generate(desiredArrayLength);
             copyOfArray = this.array.clone();
@@ -37,6 +40,7 @@ public class ArrayGenerateManager {
     }
 
     public void close(){
+        logger.warn("arrayGenerateManager is closed");
         arrayGenerateManager = null;
     }
 }
